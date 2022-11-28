7fec9db9ca3c9b94a04f234320878959d1cd5b24
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-11-28 11:45:58 -0400

    projeto atualização

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="register--container">
+        <h1> Novo cadastro</h1>
+
+        Nome completo:
+        <input
+          type="text"
+          name="nome"
+          placeholder="Nome"
+          className="register--input"
+        />
+
+        Email:
+        <input
+          type="email"
+          name="email"
+          placeholder="E-mail"
+          className="register--input"
+        />
+
+
+        Data de nascimento:
+        <input
+          type="date"
+          name="data"
+          className="register--input"
+        />
+
+        Cidade:
+        <input
+          type="text"
+          name="cidade"
+          placeholder="cidade"
+          className="register--input"
+        />
+
+        Endereço:
+        <input
+          type="text"
+          name="endereço"
+          placeholder="Endereço"
+          className="register--input"
+        />
+
+
+        Telefone:
+        <input
+          type="number"
+          name="endetelefoneeço"
+          placeholder="Telefone"
+          className="register--input"
+        />
+
+
+
+
+
+
+
+
+
+
+      </div>
+
     </div>
   );
 }
