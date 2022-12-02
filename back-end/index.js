6fb7f0f14ade665f69ff20b629f4699a34a25887
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-12-02 10:43:05 -0400

    atualização

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
-//const mysql = require("mysql");
+const mysql = require("mysql");
+
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    port: "3307",
+    password: "root159",
+    database: "cadastro"
+});
+
+app.get("/", (req, res) => {
+    let SQL = "INSERT INTO tabcadastro( nome, email, data, cidade, endereco, telefone) VALUES ('?', '?', '?', '?', '?', '?' )";
+
+    db.query(SQL, (err, result) => {
+        console.log(err)
+    });
+});
+
 
 app.get("/", (req, res) => {
     res.send("hello word")
