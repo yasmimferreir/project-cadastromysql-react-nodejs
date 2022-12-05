170c9fa438e841c50a98ff7cbff3587c07b699e2
Yasmim Ferreira Costa <97356148+yasmimferreir@users.noreply.github.com>
2022-12-05 09:30:09 -0400

    atualização

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+const cors = require("cors");
 
 
 const db = mysql.createPool({
@@ -11,18 +12,38 @@ const db = mysql.createPool({
     database: "cadastro"
 });
 
-app.get("/", (req, res) => {
-    let SQL = "INSERT INTO tabcadastro( nome, email, data, cidade, endereco, telefone) VALUES ('?', '?', '?', '?', '?', '?' )";
+app.use(cors());
+app.use(express.json());
+
+app.post("/register", (req, res) => {
+    const { nome } = req.body;
+    const { email } = req.body;
+    const { data } = req.body;
+    const { cidade } = req.body;
+    const { endereço } = req.body;
+    const { telefone } = req.body;
+
+    let SQL = "INSERT INTO tabcadastro ( nome, email, data, cidade, endereco, telefone) VALUES (?,?,?,?,?,?)";
+
+    db.query(SQL, [nome, email, data, cidade, endereço, telefone], (err, result) => {
+        console.log(err);
+    });
+});
+
+app.get("/getCards", (req, res) => {
+
+    let SQL = "SELECT * from tabcadastro";
 
     db.query(SQL, (err, result) => {
-        console.log(err)
+        if (err) console.log(err)
+        else res.send(result);
     });
 });
 
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
     res.send("hello word")
-});
+});*/
 
 app.listen(3001, () => {
     console.log("🛰️ Rodando servidor")
